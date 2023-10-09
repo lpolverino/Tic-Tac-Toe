e323b975adc1a8062870461c627727bd3ecc49f4
lpolverino <lorenz.polverino@gmail.com>
2023-10-09 15:56:38 -0300

    created screenupdater module for modularize the update of the screen

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const gameboard = ( function(doc){
     const gridElement = doc.getElementsByClassName('gameboard')[0];
-    let gridValues = Array(8).fill('')
-    gridValues.push("X")
+    let gridValues = Array(9).fill('')
 
     const createButton = (grid, val, index) =>{
         const gameboardButton = doc.createElement("div");
         gameboardButton.classList.add("gameboard-button");
-        gameboardButton.classList.add(index);
 
         const button = doc.createElement("button");
         button.innerText = val;
+        button.dataset.position = index;
+
+        button.addEventListener("click", (event)=>{
+            event.target.dataset
+        })
 
         gameboardButton.appendChild(button);
         grid.appendChild(gameboardButton);
@@ -21,11 +24,12 @@ const gameboard = ( function(doc){
         })
     }
 
-    render()
-   
-
     const changePosition = (position, value) => {
-        grid[position] = value
+        gridValues[position] = value
+    }
+
+    const validPosition = (position) =>{
+        return gridValues[position] === ''
     }
 
     const checkWinCondition = () =>{
@@ -35,49 +39,86 @@ const gameboard = ( function(doc){
     return{
         changePosition,
         checkWinCondition,
-        render
+        render,
+        validPosition
     }
 }) (document) ;
 
 
-
-function createPlayer(number){
-    const playerNumber = number
-    const player = () => {
-        return playerNumber
-    }
-    return {
-        player
-    }
+function createPlayer(number,mark){
+    return(
+        {
+            number,
+            mark
+        }
+    )
 }
 
-const playerOne = createPlayer(1)
-const playerTwo = createPlayer(2)
 
-const displayControler = (function (doc, playerOne, playerTwo){
-    let first = playerOne;
-    let second = playerTwo;
+const displayControler = (function (doc){
     const label = doc.getElementsByClassName("player-label")[0];
     
-    const render = (labelElement) =>{
-        const text = labelElement.childNodes[1];
-        text.innerText = "It's Player " + first.player() + " Turn"
-
+    const render = (player) =>{
+        const text = label.childNodes[1];
+        text.innerText = "It's Player " + player + " Turn"
     }
 
-    render(label)
-
-    const changePlayer = () => {
-        let swapPlaceholder = first
-        first = second;
-        second = swapPlaceholder;
-        render(label)
-    }
-    const getCurrentPLayer = () =>{
-        return first
-    }
     return{
-        changePlayer,
-        getCurrentPLayer,
+        render
     }
-})(document, playerOne, playerTwo);
+})(document);
+
+
+const gameControler = (function (doc,gameboard){
+
+    const players = [
+        createPlayer(1,"X"),
+        createPlayer(2,"0")
+    ]
+    let current = 0
+
+    const passTurn = () =>{
+        current = (current + 1) % 2
+    }
+
+    const playRound = (position) =>{
+        gameboard.changePosition(position, players[current].mark);
+        passTurn();
+     
+    }
+    const getCurrentPlayer = () =>{
+        return players[current]
+    }
+
+    return{
+        playRound,
+        getCurrentPlayer
+    }
+
+})(document, gameboard);
+
+
+const screenControler = (function(doc, gameboard, displayControler,gameControler){
+
+    const gridElement = doc.getElementsByClassName('gameboard')[0];
+
+    const updateScreen=()=>{
+        gameboard.render();
+        displayControler.render(gameControler.getCurrentPlayer());
+    }
+
+    const clickHandlerRound = (event) =>{
+        const selectedCell = event.target.dataset.position 
+
+        if (!selectedCell) return;
+
+        if(!gameboard.validPosition(selectedCell)) return;
+
+        gameControler.playRound(selectedCell);
+        updateScreen();
+    }
+
+    gridElement.addEventListener("click", clickHandlerRound)
+
+    updateScreen();
+})(document,gameboard,displayControler,gameControler);
